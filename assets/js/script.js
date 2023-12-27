dfa4cfc78f9a2a8c55f6105c7a57b64d7e3cdfad
douglas <douglasmaxton@gmail.com>
2023-12-27 18:58:17 +0000

    gets data from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
-import { API_KEY } from "./keys.js";
+import { keys } from "./keys.js";
 
-fetch(`https://ci-jshint.herokuapp.com/api?api_key=${API_KEY}`)
-  .then((response) => response.text())
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+// environment variables
+const { api_key, api_url } = keys;
+
+const resultsModal = new bootstrap.Modal(
+  document.getElementById("resultsModal"),
+);
+
+document
+  .getElementById("status")
+  .addEventListener("click", (e) => getStatus(e));
+
+async function getStatus(e) {
+  const queryString = `${api_url}?api_key=${api_key}`;
+  const response = await fetch(queryString);
+  const data = await response.json();
+
+  if (response.ok) {
+    console.log(data.expiry);
+  } else {
+    throw new Error(data.error);
+  }
+}
